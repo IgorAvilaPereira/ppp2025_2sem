093064c680a0c3aad40ac4818cd232b22cfe311b
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2025-08-14 09:48:01 -0300

    atualizando

--- 01_strategy/aula/strategy/src/main/java/negocio/som/QuackEstiloso.java
+++ 01_strategy/aula/strategy/src/main/java/negocio/som/QuackEstiloso.java
@@ -1,68 +1,35 @@
 package negocio.som;
 
-import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-
-import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import javax.sound.sampled.DataLine;
-import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.SourceDataLine;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class QuackEstiloso implements ComportamentoSom {
 
+    private void playSound(String filePath){
+        try {
+            File audioFile = new File(filePath);
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();
+            System.out.println("Playing audio...");            
+            // Keep the program running until the audio finishes
+            Thread.sleep(clip.getMicrosecondLength() / 1000);
+            clip.close();
+            audioStream.close();
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException | InterruptedException e) {
+            e.printStackTrace();
+        }    
+    }
+
     @Override
     public void emitirSom() {
         System.out.println("som estiloso!");
-        // // Play Audio File
-        // try {
-        // String audioFileDirectory =
-        // "/home/iapereira/git/ppp2025_2sem/01_strategy/aula/strategy/src/main/resources/quack.mp3";
-        // InputStream is = getClass().getResourceAsStream(audioFileDirectory);
-        // BufferedInputStream bis = new BufferedInputStream(is);
-        // AudioInputStream ais = AudioSystem.getAudioInputStream(bis);
-        // Clip audio = AudioSystem.getClip();
-        // audio.open(ais);
-        // FloatControl volume =
-        // (FloatControl)audio.getControl(FloatControl.Type.MASTER_GAIN);
-        // volume.setValue(1.0f); // Reduce volume by 10 decibels.
-
-        // audio.loop(Clip.LOOP_CONTINUOUSLY);
-        // audio.start();
-        // // Thread.sleep(audio.getMicrosecondLength() / 10000);
-        // audio.close();
-        // } catch (IOException | UnsupportedAudioFileException |
-        // LineUnavailableException e) {
-        // System.out.println(e.getMessage());
-
-        // }
+        playSound("./src/main/resources/quack.wav");
     }
-    // // Play Audio File
-    // try {
-    // String audioFileDirectory =
-    // "/home/iapereira/git/ppp2025_2sem/01_strategy/aula/strategy/src/main/resources/quack.mp3";
-    // InputStream is = getClass().getResourceAsStream(audioFileDirectory);
-    // BufferedInputStream bis = new BufferedInputStream(is);
-    // AudioInputStream ais = AudioSystem.getAudioInputStream(bis);
-    // Clip audio = AudioSystem.getClip();
-    // audio.open(ais);
-    // FloatControl volume =
-    // (FloatControl)audio.getControl(FloatControl.Type.MASTER_GAIN);
-    // volume.setValue(1.0f); // Reduce volume by 10 decibels.
-
-    // audio.loop(Clip.LOOP_CONTINUOUSLY);
-    // audio.start();
-    // // Thread.sleep(audio.getMicrosecondLength() / 10000);
-    // audio.close();
-    // } catch (IOException | UnsupportedAudioFileException |
-    // LineUnavailableException e) {
-    // System.out.println(e.getMessage());
-
-    // }
 }
