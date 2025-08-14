b39395543fa5e219b956641599d909bcec9c49d9
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2025-08-13 21:37:31 -0300

    atualizando

--- 01_strategy/aula/strategy/src/main/java/negocio/som/QuackEstiloso.java
+++ 01_strategy/aula/strategy/src/main/java/negocio/som/QuackEstiloso.java
@@ -1,45 +1,68 @@
 package negocio.som;
 
-import java.applet.Applet;
-import java.applet.AudioClip;
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.io.InputStream;
 
+import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
 import javax.sound.sampled.UnsupportedAudioFileException;
-import javax.swing.JOptionPane;
-import javax.swing.SwingUtilities;
 
 public class QuackEstiloso implements ComportamentoSom {
 
     @Override
     public void emitirSom() {
         System.out.println("som estiloso!");
-        // Clip sound;
+        // // Play Audio File
         // try {
-        //     sound = AudioSystem.getClip();
-        //     sound.open(AudioSystem.getAudioInputStream(new File(
-        //             "/home/iapereira/git/ppp2025_2sem/01_strategy/aula/strategy/src/main/resources/quack.wav")));
-        //     while (true) {
-        //         sound.start();
-        //     }
-        //     // Thread.sleep(2000);
-        // } catch (LineUnavailableException e) {
-        //     // TODO Auto-generated catch block
-        //     e.printStackTrace();
-        // } catch (IOException e) {
-        //     // TODO Auto-generated catch block
-        //     e.printStackTrace();
-        // } catch (UnsupportedAudioFileException e) {
-        //     // TODO Auto-generated catch block
-        //     e.printStackTrace();
+        // String audioFileDirectory =
+        // "/home/iapereira/git/ppp2025_2sem/01_strategy/aula/strategy/src/main/resources/quack.mp3";
+        // InputStream is = getClass().getResourceAsStream(audioFileDirectory);
+        // BufferedInputStream bis = new BufferedInputStream(is);
+        // AudioInputStream ais = AudioSystem.getAudioInputStream(bis);
+        // Clip audio = AudioSystem.getClip();
+        // audio.open(ais);
+        // FloatControl volume =
+        // (FloatControl)audio.getControl(FloatControl.Type.MASTER_GAIN);
+        // volume.setValue(1.0f); // Reduce volume by 10 decibels.
+
+        // audio.loop(Clip.LOOP_CONTINUOUSLY);
+        // audio.start();
+        // // Thread.sleep(audio.getMicrosecondLength() / 10000);
+        // audio.close();
+        // } catch (IOException | UnsupportedAudioFileException |
+        // LineUnavailableException e) {
+        // System.out.println(e.getMessage());
+
         // }
-
     }
+    // // Play Audio File
+    // try {
+    // String audioFileDirectory =
+    // "/home/iapereira/git/ppp2025_2sem/01_strategy/aula/strategy/src/main/resources/quack.mp3";
+    // InputStream is = getClass().getResourceAsStream(audioFileDirectory);
+    // BufferedInputStream bis = new BufferedInputStream(is);
+    // AudioInputStream ais = AudioSystem.getAudioInputStream(bis);
+    // Clip audio = AudioSystem.getClip();
+    // audio.open(ais);
+    // FloatControl volume =
+    // (FloatControl)audio.getControl(FloatControl.Type.MASTER_GAIN);
+    // volume.setValue(1.0f); // Reduce volume by 10 decibels.
 
+    // audio.loop(Clip.LOOP_CONTINUOUSLY);
+    // audio.start();
+    // // Thread.sleep(audio.getMicrosecondLength() / 10000);
+    // audio.close();
+    // } catch (IOException | UnsupportedAudioFileException |
+    // LineUnavailableException e) {
+    // System.out.println(e.getMessage());
+
+    // }
 }
